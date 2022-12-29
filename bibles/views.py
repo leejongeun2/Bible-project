b29597dfc6a45cff7bdc215a4886b574e4a2c194
tmsnvl95 <tmsnvl95@naver.com>
2022-12-29 11:45:04 +0900

    csv파일 db넣기-ing

--- bibles/views.py
+++ bibles/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from .models import Item
+from bibles.models import Item
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.core.paginator import Paginator
@@ -9,29 +9,26 @@ import csv
 # Create your views here.
 
 def index(request):
-    return render(request, "bibles/index.html")
-    # item = Item.objects.all()
+    item = Item.objects.all()
 
-    # if len(item) == 0:
-    #     items_csv = open("products/shopping3.csv", encoding="UTF-8")
-    #     items_reader = csv.reader(items_csv)
-    #     bulk_list = []
-    #     for i in items_reader:
-    #         bulk_list.append(
-    #             Item(
-    #                 title=i[0],
-    #                 price=i[1],
-    #                 image_url=i[2],
-    #             )
-    #         )
-    #     Item.objects.bulk_create(bulk_list)
-    # # pagination
-    # ordered_item = Item.objects.order_by("pk")
-    # paginator = Paginator(ordered_item, 9)
-    # page_number = request.GET.get("page")
-    # page_obj = paginator.get_page(page_number)
-    # context = {
-    #     "item": item,
-    #     "page_obj": page_obj,
-    # }
-    # return render(request, "products/index.html", context)
+    if len(item) == 0:
+        items_csv = open("bibles/bible1.csv", encoding="UTF-8")
+        items_reader = csv.reader(items_csv)
+        bulk_list = []
+        for i in items_reader:
+            bulk_list.append(
+                Item(
+                    content=i[0],
+                    content_en=i[1],
+                    vers=i[2],
+                    vers_en=i[3],
+                    keyword=i[4],
+                )
+            )
+        Item.objects.bulk_create(bulk_list)
+
+    context = {
+        "item": item,
+    }
+    return render(request, "bibles/index.html")
+    
