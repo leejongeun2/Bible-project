393a75437a06c686b88d6d6c95f16247076409b6
tmsnvl95 <tmsnvl95@naver.com>
2023-01-09 18:39:59 +0900

    url

--- pjt/urls.py
+++ pjt/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bibles/', include('bibles.urls')),
+    path('', include('bibles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
